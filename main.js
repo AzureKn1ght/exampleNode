c634d690c2e987b9af3f8d2b0d3857a293ef9133
Philson Nah <philsonnah@msn.com>
2021-07-02 16:47:41 +0800

    Added some frontend stuff

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 const product = require("./product");
 const express = require("express");
+const cors = require("cors");
+//const body_parser = require("body-parser");
 
 //Define Express functions
 server = express();
-router = express.Router();
+server.use(express.json()); //body_parser deprecated
+server.use(cors());
 
 //Define GET URI and function
 //Always has request & response params
-router.get("/welcome", (request, response) => {
-  console.log("GET called");
+// router.get("/welcome", (request, response) => {
+//   console.log("GET called");
 
-  //Default is 200, no need status()
-  response.status(200).send("HELLO WORLD");
-});
+//   //Default is 200, no need status()
+//   response.status(200).send("HELLO WORLD");
+// });
 
-server.use(router);
+//server.use(router);
+server.use(product.router);
 
 //Start the server
 //Port 3000, callback is optional
--- product.js
+++ product.js
@@ -1,22 +1,120 @@
 const database = require("./database");
+const express = require("express");
 
-function get_all_products() {
-  database.q("select * from products");
-}
+router = express.Router();
 
-function get_product_by_id(id) {
-  database.q(`
+router.get("/products/all", (req, res) => {
+  let x = "select * from products";
+  database.connection.query(x, (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("Server Error:\n" + error);
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
+
+router.get("/products/by-id", (req, res) => {
+  let id = req.query.id; //get id from request
+  let x = `
     select * from products
     where id = ${id}
-    `);
-}
+    `;
+  database.connection.query(x, (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("Server Error:\n" + error);
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
 
-function create_new_product(name, price) {
-  database.q(`
+router.post("/products/add", (req, res) => {
+  //get id from request BODY
+  let name = req.body.name;
+  let price = req.body.price;
+  let x = `
     insert into products (name, market_price)
     values ('${name}', '${price}')
-    `);
+    `;
+  database.connection.query(x, (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("Server Error:\n" + error);
+    } else {
+      res.status(200).send("New product created successfully!");
+    }
+  });
+});
+
+router.put("/products/update/by-id", (req, res) => {
+  //get id from request BODY
+  let id = req.body.id;
+  let name = req.body.new_name;
+  let price = req.body.new_price;
+  let x = `
+    update products
+    set name = '${name}',
+    market_price = '${price}'
+    where id = ${id}
+    `;
+  database.connection.query(x, (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("Server Error:\n" + error);
+    } else {
+      res.status(200).send("Updated Successfully!");
+    }
+  });
+});
+
+router.delete("/products/delete/by-id", (req, res) => {
+  let id = req.query.id; //get id from request
+  let x = `
+    delete from products
+    where id = ${id}
+    `;
+  database.connection.query(x, (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("Server Error:\n" + error);
+    } else {
+      res.status(200).send("Deleted Successfully!");
+    }
+  });
+});
+
+function q(query) {
+  var res = false;
+  database.connection.query(query, (error, results) => {
+    if (error) {
+      console.error(error);
+    } else {
+      res = results;
+    }
+  });
+  return res;
 }
+
+// function get_all_products() {
+//   database.q("select * from products");
+// }
+
+// function get_product_by_id(id) {
+//   database.q(`
+//     select * from products
+//     where id = ${id}
+//     `);
+// }
+
+// function create_new_product(name, price) {
+//   database.q(`
+//     insert into products (name, market_price)
+//     values ('${name}', '${price}')
+//     `);
+// }
 
 function delete_product_by_id(id) {
   database.q(`
@@ -25,18 +123,19 @@ function delete_product_by_id(id) {
     `);
 }
 
-function update_price_by_id(id, price) {
-  database.q(`
-    update products
-    set market_price = ${price}
-    where id = ${id}
-    `);
-}
+// function update_price_by_id(id, price) {
+//   database.q(`
+//     update products
+//     set market_price = ${price}
+//     where id = ${id}
+//     `);
+// }
 
 module.exports = {
-  get_all_products,
-  get_product_by_id,
-  create_new_product,
-  delete_product_by_id,
-  update_price_by_id,
+  //get_all_products,
+  //get_product_by_id,
+  //create_new_product,
+  //delete_product_by_id,
+  //update_price_by_id,
+  router,
 };
